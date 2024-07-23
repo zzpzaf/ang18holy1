83a3523e223ad21f660f9adeed736eca5e9cc734
Panos Zafeiropoulos <zzpzaf@gmail.com>
2024-07-23 17:03:03 +0200

    Started using CSS variables and passing them values from componet clas variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NavrowComponent } from './navrow/navrow.component';
@@ -20,8 +20,38 @@ import { FooterComponent } from './footer/footer.component';
     RouterOutlet
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular18-pure-repo1';
+
+  allPadding: number = 6;
+
+  hhLarge = 80;
+  nrhLarge  = 50;
+  fhLarge  = 60;
+
+
+  leftp_Width = 200;
+  main_Width = 0; 
+  rightp_Width = 200;
+
+
+  get allPaddingStyle() {
+    return `${this.allPadding}px`;
+  }
+
+  get hhLargeStyle() {
+    return `${this.hhLarge}px`;
+  }
+
+  get nrhLargeStyle() {
+    return `${this.nrhLarge}px`;
+  }
+
+  get fhLargeStyle() {
+    return `${this.fhLarge}px`;
+  }
+
+  
 }
