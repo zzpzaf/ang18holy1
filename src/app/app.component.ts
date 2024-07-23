d0f3ae5d29a4226eb0941ddcdb8654bd1c32fc56
Panos Zafeiropoulos <zzpzaf@gmail.com>
2024-07-23 19:41:21 +0200

    Refactoring. All variables are passed in html template and stylesheet via an Object (key-values) in AppComponent class.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NavrowComponent } from './navrow/navrow.component';
@@ -6,6 +6,10 @@ import { LeftpaneComponent } from './leftpane/leftpane.component';
 import { MainComponent } from './main/main.component';
 import { RightpaneComponent } from './rightpane/rightpane.component';
 import { FooterComponent } from './footer/footer.component';
+
+interface LayoutConfig {
+  [key: string]: { value: number | string; unit: string };
+}
 
 @Component({
   selector: 'app-root',
@@ -25,33 +29,34 @@ import { FooterComponent } from './footer/footer.component';
 export class AppComponent {
   title = 'angular18-pure-repo1';
 
-  allPadding: number = 6;
+  private layoutConfig: LayoutConfig = {
+    allPadding: { value: 6, unit: 'px' },
 
-  hhLarge = 80;
-  nrhLarge  = 50;
-  fhLarge  = 60;
+    hhSmall: { value:65, unit: 'px' },
+    nrhSmall: { value: 'min-content', unit: '' },
+    lphSmall: { value: 1, unit: 'fr' },
+    mhSmall: { value: 1.7, unit: 'fr' },
+    rphSmall: { value:'min-content', unit: '' },
+    fhSmall: { value: 'min-content', unit: '' },
 
+    hhMedium: { value:70, unit: 'px' },
+    nrhMedium: { value: 40, unit: 'px' },
+    rphMedium: { value:'min-content', unit: '' },
+    fhMedium: { value: 50, unit: 'px' },
+    lpw1Medium: { value: 180, unit: 'px' },
+    lpw2Medium: { value: 20, unit: '%' },
 
-  leftp_Width = 200;
-  main_Width = 0; 
-  rightp_Width = 200;
+    hhLarge: { value:80, unit: 'px' },
+    nrhLarge: { value: 50, unit: 'px' },
+    fhLarge: { value: 60, unit: 'px' },
+    lpw1Large: { value: 200, unit: 'px' },
+    lpw2Large: { value: 25, unit: '%' },
+    rpwLarge: { value: 'min-content', unit: '' }
+  };
 
-
-  get allPaddingStyle() {
-    return `${this.allPadding}px`;
+  get layoutConfigStyle(): string {
+    return Object.entries(this.layoutConfig)
+      .map(([key, { value, unit }]) => `--${key}: ${value}${unit}`)
+      .join(';');
   }
-
-  get hhLargeStyle() {
-    return `${this.hhLarge}px`;
-  }
-
-  get nrhLargeStyle() {
-    return `${this.nrhLarge}px`;
-  }
-
-  get fhLargeStyle() {
-    return `${this.fhLarge}px`;
-  }
-
-  
 }
