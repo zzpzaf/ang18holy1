e614b9fad8f79fb81a59d16e49bcc2e54d4b518a
Panos Zafeiropoulos <zzpzaf@gmail.com>
2024-08-05 12:01:11 +0200

    A 1st attempt to use the mat-grid-list as the basis of a holy-grail layout implementation
    I created a new branch -the ‘matgridlist’ branch for the commit that only pure Angular Material has had just installed.
    
    The most important challenge was the alignment of items inside a mat-grid-tile.
    A working solution was to use a <span> element inside the mat-grid-tile, and assign to that span the specific class. Then in the specific class I used a flex container. Note, that font-size and color (background-color) was working OK, but to achieve a desirable alignment of the component enclosed, I had to set height and width to 100%.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 import { HeaderComponent } from './header/header.component';
 import { NavrowComponent } from './navrow/navrow.component';
 import { LeftpaneComponent } from './leftpane/leftpane.component';
@@ -15,6 +17,7 @@ interface LayoutConfig {
   selector: 'app-root',
   standalone: true,
   imports: [
+    MatGridListModule,
     HeaderComponent,
     NavrowComponent,
     LeftpaneComponent,
@@ -27,36 +30,41 @@ interface LayoutConfig {
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Angular18-Holy-Grail-repo1(Grid)';
+  title = 'Angular18-Holy-Grail based on mat-grid-list';
 
-  private layoutConfig: LayoutConfig = {
-    allPadding: { value: 6, unit: 'px' },
+  // private layoutConfig: LayoutConfig = {
+  //   allPadding: { value: 6, unit: 'px' },
 
-    hhSmall: { value:65, unit: 'px' },
-    nrhSmall: { value: 'min-content', unit: '' },
-    lphSmall: { value: 1, unit: 'fr' },
-    mhSmall: { value: 1.7, unit: 'fr' },
-    rphSmall: { value:'min-content', unit: '' },
-    fhSmall: { value: 'min-content', unit: '' },
+  //   hhSmall: { value:65, unit: 'px' },
+  //   nrhSmall: { value: 'min-content', unit: '' },
+  //   lphSmall: { value: 1, unit: 'fr' },
+  //   mhSmall: { value: 1.7, unit: 'fr' },
+  //   rphSmall: { value:'min-content', unit: '' },
+  //   fhSmall: { value: 'min-content', unit: '' },
 
-    hhMedium: { value:70, unit: 'px' },
-    nrhMedium: { value: 40, unit: 'px' },
-    rphMedium: { value:'min-content', unit: '' },
-    fhMedium: { value: 50, unit: 'px' },
-    lpw1Medium: { value: 180, unit: 'px' },
-    lpw2Medium: { value: 20, unit: '%' },
+  //   hhMedium: { value:70, unit: 'px' },
+  //   nrhMedium: { value: 40, unit: 'px' },
+  //   rphMedium: { value:'min-content', unit: '' },
+  //   fhMedium: { value: 50, unit: 'px' },
+  //   lpw1Medium: { value: 180, unit: 'px' },
+  //   lpw2Medium: { value: 20, unit: '%' },
 
-    hhLarge: { value:80, unit: 'px' },
-    nrhLarge: { value: 50, unit: 'px' },
-    fhLarge: { value: 60, unit: 'px' },
-    lpw1Large: { value: 200, unit: 'px' },
-    lpw2Large: { value: 25, unit: '%' },
-    rpwLarge: { value: 'min-content', unit: '' }
-  };
+  //   hhLarge: { value:80, unit: 'px' },
+  //   nrhLarge: { value: 50, unit: 'px' },
+  //   fhLarge: { value: 60, unit: 'px' },
+  //   lpw1Large: { value: 200, unit: 'px' },
+  //   lpw2Large: { value: 25, unit: '%' },
+  //   rpwLarge: { value: 'min-content', unit: '' }
+  // };
 
-  get layoutConfigStyle(): string {
-    return Object.entries(this.layoutConfig)
-      .map(([key, { value, unit }]) => `--${key}: ${value}${unit}`)
-      .join(';');
-  }
+  // get layoutConfigStyle(): string {
+  //   return Object.entries(this.layoutConfig)
+  //     .map(([key, { value, unit }]) => `--${key}: ${value}${unit}`)
+  //     .join(';');
+  // }
+
+
+  
+
+
 }
